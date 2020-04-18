5002b625452df420da76683ce8c5773413df8f25
kai <zk.zhengkai@outlook.com>
2020-04-18 18:12:51 -0400

    add navigation control

--- app/src/main/java/com/example/tinnews/MainActivity.java
+++ app/src/main/java/com/example/tinnews/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.tinnews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+
+        // connect the view in graph
+        NavigationUI.setupWithNavController(navView, navController);
+        // set action bar name to change with the navigation view
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        // allow us to return to HOME
+        return navController.navigateUp();
     }
 }
