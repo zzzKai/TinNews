bc47b63acb98f17d970b7c6d6e1f4b737eb04807
kai <zk.zhengkai@outlook.com>
2020-04-18 18:44:39 -0400

    add article model

---
+++ app/src/main/java/com/example/tinnews/model/Article.java
@@ -0,0 +1,12 @@
+package com.example.tinnews.model;
+
+public class Article {
+    public Source source;
+    public String author;
+    public String content;
+    public String description;
+    public String publishedAt;
+    public String title;
+    public String url;
+    public String urlToImage;
+}
