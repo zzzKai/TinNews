b9c1d0566730a7f03ea00d46eca05ca055336b28
kai <zk.zhengkai@outlook.com>
2020-04-18 18:44:51 -0400

    add news response model

---
+++ app/src/main/java/com/example/tinnews/model/NewsResponse.java
@@ -0,0 +1,11 @@
+package com.example.tinnews.model;
+
+import java.util.List;
+
+public class NewsResponse {
+    public Integer totalResults;
+    public List<Article> articles;
+    public String code;
+    public String message;
+    public String status;
+}
