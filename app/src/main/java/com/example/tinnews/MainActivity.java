7f2cfcca51fe5d6777fa2bbcb727fe72ad2118b5
kai <zk.zhengkai@outlook.com>
2020-04-18 19:34:58 -0400

    add test case

--- app/src/main/java/com/example/tinnews/MainActivity.java
+++ app/src/main/java/com/example/tinnews/MainActivity.java
@@ -6,8 +6,16 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.tinnews.model.NewsResponse;
+import com.example.tinnews.network.NewsApi;
+import com.example.tinnews.network.RetrofitClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +34,25 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
         // set action bar name to change with the navigation view
         NavigationUI.setupActionBarWithNavController(this, navController);
+
+        NewsApi api = RetrofitClient.newInstance(this).create(NewsApi.class);
+
+        api.getTopHeadlines("US").enqueue(new Callback<NewsResponse>() {
+            @Override
+            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+                if (response.isSuccessful()) {
+                    Log.d("getTopHeadlines", response.body().toString());
+                } else {
+                    Log.d("getTopHeadlines", response.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<NewsResponse> call, Throwable t) {
+                Log.d("getTopHeadlines", t.toString());
+            }
+        });
+
     }
 
     @Override
