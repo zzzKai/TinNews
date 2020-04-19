719902d854507505e1b4d4b458ac9f8b75a84f75
kai <zk.zhengkai@outlook.com>
2020-04-19 19:03:02 -0400

    add StethoInterceptor

--- app/src/main/java/com/example/tinnews/network/RetrofitClient.java
+++ app/src/main/java/com/example/tinnews/network/RetrofitClient.java
@@ -3,6 +3,7 @@ package com.example.tinnews.network;
 import android.content.Context;
 
 import com.ashokvarma.gander.GanderInterceptor;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 
 import java.io.IOException;
 
@@ -22,6 +23,7 @@ public class RetrofitClient {
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .addInterceptor(new HeaderInterceptor())
                 .addInterceptor(new GanderInterceptor(context).showNotification(true))
+                .addNetworkInterceptor(new StethoInterceptor())
                 .build();
         return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
