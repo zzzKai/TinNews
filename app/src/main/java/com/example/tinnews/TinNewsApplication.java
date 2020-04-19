927959947df1e5cf2f98600645f6cc3457508742
kai <zk.zhengkai@outlook.com>
2020-04-19 19:05:03 -0400

    add stetho dev tool

--- app/src/main/java/com/example/tinnews/TinNewsApplication.java
+++ app/src/main/java/com/example/tinnews/TinNewsApplication.java
@@ -4,12 +4,16 @@ import android.app.Application;
 
 import com.ashokvarma.gander.Gander;
 import com.ashokvarma.gander.imdb.GanderIMDB;
+import com.facebook.stetho.Stetho;
 
 public class TinNewsApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // debugger using
         Gander.setGanderStorage(GanderIMDB.getInstance());
+        // dev tool
+        Stetho.initializeWithDefaults(this);
     }
 }
