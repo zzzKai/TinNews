9a92a926256737610358ec9718cbf6b722f43fde
kai <zk.zhengkai@outlook.com>
2020-04-19 18:36:02 -0400

    add news view model factory

---
+++ app/src/main/java/com/example/tinnews/repository/NewsViewModelFactory.java
@@ -0,0 +1,29 @@
+package com.example.tinnews.repository;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.tinnews.ui.home.HomeViewModel;
+import com.example.tinnews.ui.search.SearchViewModel;
+
+public class NewsViewModelFactory implements ViewModelProvider.Factory {
+
+    private final NewsRepository repository;
+
+    public NewsViewModelFactory(NewsRepository repository) {
+        this.repository = repository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(HomeViewModel.class)) {
+            return (T) new HomeViewModel(repository);
+        } else if (modelClass.isAssignableFrom(SearchViewModel.class)) {
+            return (T) new SearchViewModel(repository);
+        } else {
+            throw new IllegalStateException("Unknown ViewModel");
+        }
+    }
+}
