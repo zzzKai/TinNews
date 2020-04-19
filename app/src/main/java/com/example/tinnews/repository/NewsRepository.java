1aa805561d22800931ff4454e064b19422b5200f
kai <zk.zhengkai@outlook.com>
2020-04-19 18:17:03 -0400

    add getTopHeadlines function

--- app/src/main/java/com/example/tinnews/repository/NewsRepository.java
+++ app/src/main/java/com/example/tinnews/repository/NewsRepository.java
@@ -2,13 +2,46 @@ package com.example.tinnews.repository;
 
 import android.content.Context;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.tinnews.model.NewsResponse;
 import com.example.tinnews.network.NewsApi;
 import com.example.tinnews.network.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class NewsRepository {
     private final NewsApi newsApi;
 
     public NewsRepository(Context context) {
         newsApi = RetrofitClient.newInstance(context).create(NewsApi.class);
+    }
+
+
+    public LiveData<NewsResponse> getTopHeadlines(String country) {
+        MutableLiveData<NewsResponse> topHeadlinesLiveData = new MutableLiveData<>();
+
+        newsApi.getTopHeadlines(country)
+                .enqueue(new Callback<NewsResponse>() {
+                    @Override
+                    public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+                        if (response.isSuccessful()) {
+                            topHeadlinesLiveData.setValue(response.body());
+                        } else {
+                            topHeadlinesLiveData.setValue(null);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<NewsResponse> call, Throwable t) {
+                        // 300 400
+                        topHeadlinesLiveData.setValue(null);
+                    }
+                });
+
+        return topHeadlinesLiveData;
     }
 }
