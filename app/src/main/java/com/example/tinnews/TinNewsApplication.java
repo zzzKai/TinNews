19bb7f1588fd584ef890eefa99db97cbc54e9a2a
kai <zk.zhengkai@outlook.com>
2020-04-25 18:51:01 -0400

    add database

--- app/src/main/java/com/example/tinnews/TinNewsApplication.java
+++ app/src/main/java/com/example/tinnews/TinNewsApplication.java
@@ -2,11 +2,16 @@ package com.example.tinnews;
 
 import android.app.Application;
 
+import androidx.room.Room;
+
 import com.ashokvarma.gander.Gander;
 import com.ashokvarma.gander.imdb.GanderIMDB;
+import com.example.tinnews.database.AppDatabase;
 import com.facebook.stetho.Stetho;
 
 public class TinNewsApplication extends Application {
+
+    private static AppDatabase database;
 
     @Override
     public void onCreate() {
@@ -15,5 +20,12 @@ public class TinNewsApplication extends Application {
         Gander.setGanderStorage(GanderIMDB.getInstance());
         // dev tool
         Stetho.initializeWithDefaults(this);
+        database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "tin_db").build();
     }
+
+    public static AppDatabase getDatabase() {
+        return database;
+    }
+
+
 }
