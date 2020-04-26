05f2839a2c2841852df3748a58b9ad80e3731b3f
kai <zk.zhengkai@outlook.com>
2020-04-26 19:26:18 -0400

    add Listener functions

--- app/src/main/java/com/example/tinnews/ui/search/SearchViewModel.java
+++ app/src/main/java/com/example/tinnews/ui/search/SearchViewModel.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
+import com.example.tinnews.model.Article;
 import com.example.tinnews.model.NewsResponse;
 import com.example.tinnews.repository.NewsRepository;
 
@@ -12,6 +13,7 @@ public class SearchViewModel extends ViewModel {
 
     private final NewsRepository repository;
     private final MutableLiveData<String> searchInput = new MutableLiveData<>();
+    private final MutableLiveData<Article> favoriteArticleInput = new MutableLiveData<>();
 
     public SearchViewModel(NewsRepository repository) {
         this.repository = repository;
@@ -23,6 +25,19 @@ public class SearchViewModel extends ViewModel {
 
     public LiveData<NewsResponse> searchNews() {
         return Transformations.switchMap(searchInput, repository::searchNews);
+
+    }
+
+    public void setFavoriteArticleInput(Article news) {
+        favoriteArticleInput.setValue(news);
+    }
+
+    public LiveData<Boolean> onFavorite() {
+        return Transformations.switchMap(favoriteArticleInput, repository::favoriteArticle);
+    }
+
+    public void onCancel() {
+        repository.onCancel();
     }
 
 }
