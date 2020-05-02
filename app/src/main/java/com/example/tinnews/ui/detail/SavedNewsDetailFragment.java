8fb226ee78f8d9b366c204bfb25176fc1141be7a
kai <zk.zhengkai@outlook.com>
2020-05-02 17:59:23 -0400

    get the article data and print to logcat

--- app/src/main/java/com/example/tinnews/ui/detail/SavedNewsDetailFragment.java
+++ app/src/main/java/com/example/tinnews/ui/detail/SavedNewsDetailFragment.java
@@ -2,13 +2,17 @@ package com.example.tinnews.ui.detail;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tinnews.R;
+import com.example.tinnews.model.Article;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,4 +30,17 @@ public class SavedNewsDetailFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_saved_news_detail, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        if (getArguments() == null) {
+            return;
+        }
+        Article article = SavedNewsDetailFragmentArgs.fromBundle(getArguments()).getArticle();
+        if (article == null) {
+            return;
+        }
+        Log.d("SaveNewsDetailFragment", article.toString());
+    }
+
 }
