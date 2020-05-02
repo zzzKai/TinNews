df823fd0de3533fec290e3cbc077650b5edc9e9f
kai <zk.zhengkai@outlook.com>
2020-05-02 12:19:45 -0400

    get all articles from db

--- app/src/main/java/com/example/tinnews/ui/save/SaveFragment.java
+++ app/src/main/java/com/example/tinnews/ui/save/SaveFragment.java
@@ -2,23 +2,30 @@ package com.example.tinnews.ui.save;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tinnews.R;
+import com.example.tinnews.repository.NewsRepository;
+import com.example.tinnews.repository.NewsViewModelFactory;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SaveFragment extends Fragment {
 
+    private SaveViewModel viewModel;
+
     public SaveFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,4 +33,28 @@ public class SaveFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_save, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NewsRepository repository = new NewsRepository(getContext());
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository)).get(SaveViewModel.class);
+        viewModel
+                .getAllSavedArticles()
+                .observe(
+                        getViewLifecycleOwner(),
+                        savedArticles -> {
+                            if (savedArticles != null) {
+                                Log.d("SaveFragment", savedArticles.toString());
+                            }
+                        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        viewModel.onCancel();
+    }
+
 }
