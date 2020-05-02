62d0fe1a58885c6bd9a7ce051cd1834a6776e109
kai <zk.zhengkai@outlook.com>
2020-05-02 17:38:31 -0400

    make article serializable

--- app/src/main/java/com/example/tinnews/model/Article.java
+++ app/src/main/java/com/example/tinnews/model/Article.java
@@ -5,10 +5,11 @@ import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
+import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-public class Article {
+public class Article implements Serializable {
     @Ignore
     public Source source;
     public String author;
